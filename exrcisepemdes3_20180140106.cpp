414bbf5e6babd778c9159421b846f65e5c605b9b
dedechndra <dedech888@gmail.com>
2023-07-21 02:31:36 +0700

    add class pengarang

--- exrcisepemdes3_20180140106.cpp
+++ exrcisepemdes3_20180140106.cpp
@@ -7,3 +7,18 @@ public:
 	string namaBuku;
 	buku(string pNamaBuku) :namaBuku(pNamaBuku) {}
 };
+
+
+class pengarang {
+public:
+	string namapengarang;
+	vector <pengarang*> daftarpengarang;
+	vector <buku*> daftarbuku;
+	pengarang(string pNama) : namapengarang(pNama) {}
+
+	void tambahpengarang(pengarang*);
+	void cetakpengarang();
+
+	void tambahbuku(buku*);
+	void cetakbuku();
+};
