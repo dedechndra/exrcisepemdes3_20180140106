dbb18568d5c4241c699d4ba3e959bb583c0ef4fd
dedechndra <dedech888@gmail.com>
2023-07-21 02:34:55 +0700

    add int main

--- exrcisepemdes3_20180140106.cpp
+++ exrcisepemdes3_20180140106.cpp
@@ -84,3 +84,44 @@ void penerbit::cetakpengarang() {
 	}
 	cout << endl;
 }
+
+
+int main() {
+	penerbit* varpenerbit1 = new penerbit("gama press");
+	penerbit* varpenerbit2 = new penerbit("intan pariwara");
+
+	pengarang* varpengarang1 = new pengarang("joko");
+	pengarang* varpengarang2 = new pengarang("lia");
+	pengarang* varpengarang3 = new pengarang("giga");
+	pengarang* varpengarang4 = new pengarang("asroni");
+
+	buku* varbuku1 = new buku("fisika");
+	buku* varbuku2 = new buku("Algoritma");
+	buku* varbuku3 = new buku("Basisdata");
+	buku* varbuku4 = new buku("Dasar Pemrograman");
+	buku* varbuku5 = new buku("Matematika");
+	buku* varbuku6 = new buku("Multimedia 1");
+
+	varpenerbit1->tambahpengarang(varpengarang1);
+	varpenerbit1->tambahpengarang(varpengarang2);
+	varpenerbit1->tambahpengarang(varpengarang3);
+
+	varpenerbit2->tambahpengarang(varpengarang4);
+	varpenerbit2->tambahpengarang(varpengarang3);
+
+	varpengarang3->tambahpengarang(varpengarang1);
+	varpengarang3->tambahpengarang(varpengarang2);
+
+	varpengarang1->tambahbuku(varbuku1);
+	varpengarang1->tambahbuku(varbuku2);
+	varpengarang2->tambahbuku(varbuku3);
+	varpengarang4->tambahbuku(varbuku4);
+	varpengarang3->tambahbuku(varbuku5);
+	varpengarang3->tambahbuku(varbuku6);
+
+	varpengarang1->cetakpengarang();
+	varpengarang2->cetakpengarang();
+	varpengarang3->cetakpengarang();
+
+	return 0;
+}
